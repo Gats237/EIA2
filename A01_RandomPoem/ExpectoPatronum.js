5e005b3b770cc6478fd9a8317adf6b8759047e0c
Gats237 <henning.silas.pils@hs-furtwangen.de>
2022-03-28 01:45:15 +0200

    Steckbrief link

--- A01_RandomPoem/ExpectoPatronum.js
+++ A01_RandomPoem/ExpectoPatronum.js
@@ -1,6 +1,23 @@
 "use strict";
 //Starte die Entwickertools und drücke F12
-let subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-let praedikate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-let objects = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+let sub = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let pra = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let obj = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+// console.log(sub);
+// console.log(pra);
+// console.log(obj);
+for (let i = 5; i >= 0; i--) {
+    let verse = getVerse(sub, pra, obj);
+    console.log(verse);
+}
+function getVerse(_sub, _pra, _obj) {
+    let subI = Math.floor(Math.random() * _sub.length);
+    let praI = Math.floor(Math.random() * _pra.length);
+    let objI = Math.floor(Math.random() * _obj.length);
+    let poem = _sub[subI] + " " + _pra[praI] + " " + _obj[objI];
+    _sub.splice(subI, 1);
+    _pra.splice(praI, 1);
+    _obj.splice(objI, 1);
+    return poem;
+}
 //# sourceMappingURL=ExpectoPatronum.js.map
--- A01_RandomPoem/ExpectoPatronum.ts
+++ A01_RandomPoem/ExpectoPatronum.ts
@@ -1,6 +1,28 @@
 //Starte die Entwickertools und drücke F12
-let  subject: string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
-let praedikate: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
-let objects: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+let sub: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let pra: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let obj: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
 
+// console.log(sub);
+// console.log(pra);
+// console.log(obj);
 
+for (let i: number = 5; i >= 0; i--) {
+    let verse: string[] = getVerse(sub, pra, obj);
+    console.log(verse);
+        }
+
+function getVerse (_sub: string[], _pra: string[], _obj: string[]): string[] {
+
+    let subI: number = Math.floor(Math.random() * _sub.length);
+    let praI: number = Math.floor(Math.random() * _pra.length);
+    let objI: number = Math.floor(Math.random() * _obj.length);
+
+    let poem: string = _sub[subI] + " " + _pra[praI] + " " + _obj[objI];
+
+    _sub.splice(subI, 1);
+    _pra.splice(praI, 1);
+    _obj.splice(objI, 1);
+    
+    return poem: string ;
+}
